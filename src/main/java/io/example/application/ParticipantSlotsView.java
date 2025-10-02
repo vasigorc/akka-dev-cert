86857fe7cb97d9228cc7448933372c23904def90
Vasile Gorcinschi <vgorcinschi@ncl.com>
2025-10-02 18:41:25 -0400

    feat: Implemented ParticipantSlotsView

--- src/main/java/io/example/application/ParticipantSlotsView.java
+++ src/main/java/io/example/application/ParticipantSlotsView.java
@@ -5,10 +5,9 @@ import akka.javasdk.annotations.Consume;
 import akka.javasdk.annotations.Query;
 import akka.javasdk.view.TableUpdater;
 import akka.javasdk.view.View;
-import io.example.application.ParticipantSlotEntity.Event.Booked;
-import io.example.application.ParticipantSlotEntity.Event.Canceled;
-import io.example.application.ParticipantSlotEntity.Event.MarkedAvailable;
-import io.example.application.ParticipantSlotEntity.Event.UnmarkedAvailable;
+import io.example.application.ParticipantSlotEntity.Event;
+import io.example.domain.Participant.ParticipantAvailabilityStatus;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,38 +15,57 @@ import org.slf4j.LoggerFactory;
 @ComponentId("view-participant-slots")
 public class ParticipantSlotsView extends View {
 
-    private static Logger logger = LoggerFactory.getLogger(ParticipantSlotsView.class);
+  private static Logger logger = LoggerFactory.getLogger(ParticipantSlotsView.class);
 
-    @Consume.FromEventSourcedEntity(ParticipantSlotEntity.class)
-    public static class ParticipantSlotsViewUpdater extends TableUpdater<SlotRow> {
+  @Consume.FromEventSourcedEntity(ParticipantSlotEntity.class)
+  public static class ParticipantSlotsViewUpdater extends TableUpdater<SlotRow> {
 
-        public Effect<SlotRow> onEvent(ParticipantSlotEntity.Event event) {
-            // Supply your own implementation
-            return effects().ignore();
-        }
+    public Effect<SlotRow> onEvent(ParticipantSlotEntity.Event event) {
+      return switch (event) {
+        case Event.MarkedAvailable marked ->
+          effects().updateRow(new SlotRow(marked.slotId(), marked.participantId(), marked.participantType().name(),
+              rowState().bookingId(), ParticipantAvailabilityStatus.AVAILABLE.getValue()));
+        case Event.UnmarkedAvailable unmarked ->
+          effects()
+              .updateRow(new SlotRow(unmarked.slotId(), unmarked.participantId(), unmarked.participantType().name(),
+                  rowState().bookingId(), ParticipantAvailabilityStatus.UNAVAILABLE.getValue()));
+        case Event.Booked booked ->
+          effects()
+              .updateRow(new SlotRow(booked.slotId(), booked.participantId(), booked.participantType().name(),
+                  booked.bookingId(), ParticipantAvailabilityStatus.UNAVAILABLE.getValue()));
+        case Event.Canceled canceled ->
+          effects()
+              .updateRow(new SlotRow(canceled.slotId(), canceled.participantId(), canceled.participantType().name(),
+                  canceled.bookingId(), ParticipantAvailabilityStatus.AVAILABLE.getValue()));
+      };
     }
+  }
 
-    public record SlotRow(
-            String slotId,
-            String participantId,
-            String participantType,
-            String bookingId,
-            String status) {
-    }
+  public record SlotRow(
+      String slotId,
+      String participantId,
+      String participantType,
+      String bookingId,
+      String status) {
+  }
 
-    public record ParticipantStatusInput(String participantId, String status) {
-    }
+  public record ParticipantStatusInput(String participantId, String status) {
+  }
 
-    public record SlotList(List<SlotRow> slots) {
-    }
+  public record SlotList(List<SlotRow> slots) {
+  }
 
-    // @Query("SELECT .... ")
-    public QueryEffect<SlotList> getSlotsByParticipant(String participantId) {
-        return queryResult();
-    }
+  @Query("SELECT * AS slots FROM  view_participant_slots WHERE participantId = :participantId")
+  public QueryEffect<SlotList> getSlotsByParticipant(String participantId) {
+    return queryResult();
+  }
 
-    // @Query("SELECT ...")
-    public QueryEffect<SlotList> getSlotsByParticipantAndStatus(ParticipantStatusInput input) {
-        return queryResult();
-    }
+  @Query("""
+      SELECT * AS slots FROM view_participant_slots
+      WHERE participantId = :participantId
+      AND status = :status
+      """)
+  public QueryEffect<SlotList> getSlotsByParticipantAndStatus(ParticipantStatusInput input) {
+    return queryResult();
+  }
 }
